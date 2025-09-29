591a87fc7cfc6f4bef82f08f1a55e130a3167584
dolphinau <alexandre.tullot@protonmail.com>
2025-09-29 11:29:35 +0200

    ignore target and build files

--- target/wasm32-wasip2/debug/deps/rust_wasm.d
+++ target/wasm32-wasip2/debug/deps/rust_wasm.d
@@ -1,6 +1,6 @@
-/home/alex/work/rust-wasm/target/wasm32-wasip2/debug/deps/rust_wasm.d: src/lib.rs /home/alex/work/rust-wasm/wit/adder.wit
+/home/alex/ghq/github.com/dolphinau/rust-wasm/target/wasm32-wasip2/debug/deps/rust_wasm.d: src/lib.rs /home/alex/ghq/github.com/dolphinau/rust-wasm/wit/adder.wit
 
-/home/alex/work/rust-wasm/target/wasm32-wasip2/debug/deps/rust_wasm.wasm: src/lib.rs /home/alex/work/rust-wasm/wit/adder.wit
+/home/alex/ghq/github.com/dolphinau/rust-wasm/target/wasm32-wasip2/debug/deps/rust_wasm.wasm: src/lib.rs /home/alex/ghq/github.com/dolphinau/rust-wasm/wit/adder.wit
 
 src/lib.rs:
-/home/alex/work/rust-wasm/wit/adder.wit:
+/home/alex/ghq/github.com/dolphinau/rust-wasm/wit/adder.wit:
--- target/wasm32-wasip2/debug/rust_wasm.d
+++ target/wasm32-wasip2/debug/rust_wasm.d
@@ -1,1 +1,1 @@
-/home/alex/work/rust-wasm/target/wasm32-wasip2/debug/rust_wasm.wasm: /home/alex/work/rust-wasm/src/lib.rs /home/alex/work/rust-wasm/wit/adder.wit
+/home/alex/ghq/github.com/dolphinau/rust-wasm/target/wasm32-wasip2/debug/rust_wasm.wasm: /home/alex/ghq/github.com/dolphinau/rust-wasm/src/lib.rs /home/alex/ghq/github.com/dolphinau/rust-wasm/wit/adder.wit
